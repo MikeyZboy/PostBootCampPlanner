0f169d1f261fffb82635225f4ae02e241459c404
Mike Zahuta <mike.zahuta@gmail.com>
2021-01-28 16:00:29 -0500

    code jwt auth middleware

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require('morgan')
 const AppRouter = require("./routes/AppRouter");
+const helmet = require('helmet')
 const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(logger('dev'))
+app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
