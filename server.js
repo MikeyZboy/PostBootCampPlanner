234c09339d2e8fd1fad95a60676cda38ecfba72d
Mike Zahuta <mike.zahuta@gmail.com>
2021-07-06 15:02:22 -0400

    update res in server

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(helmet())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(
         path.join(__dirname + '/client/build/index.html')
         );
