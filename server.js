c1d946cedc6efcff35bc458812cc0c04a1516dbf
Mike Zahuta <mike.zahuta@gmail.com>
2021-07-07 07:12:24 -0400

    update server.js for connection fe and be

--- server.js
+++ server.js
@@ -3,22 +3,31 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const logger = require('morgan')
 const AppRouter = require("./routes/AppRouter");
-const helmet = require('helmet')
+const helmet = require('helmet');
+const { Connection } = require("pg");
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 app.use(cors());
 app.use(logger('dev'))
-app.use(helmet())
+app.use(helmet({ contentSecurityPolicy: false}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.disable('X-Powered-By')
 
-app.get("/", (req, res) => {
-    res.send(__dirname + '/client/build/index.html')
-    // res.sendFile(
-    //     path.join(__dirname + '/client/build/index.html')
-    //     );
-    });
-// app.get("/", (req, res) => res.json({ message: "Server Works" }));
 app.use("/api", AppRouter);
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+);
 
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
+app.listen(PORT, async () => {
+    try {
+        await connection
+        console.log('Database connected')
+        console.log(`App Listening On Port: ${PORT}`);
+    } catch (error) {
+        throw new Error('Connection error')
+    }
+
+})
+
